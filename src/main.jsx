d8ae8c350f58f5798eddd3aff8b031ced1f53fd7
Marc <147423007+marcaixs@users.noreply.github.com>
2025-09-03 12:01:34 +0200

    add routes to pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import HomePage from './pages/HomePage.jsx';
+import ShopPage from './pages/ShopPage.jsx';
+import CartPage from './pages/CartPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "shop",
+    element: <ShopPage />,
+  },
+  {
+    path: "cart",
+    element: <CartPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
