83f6061a01df1cf31f7be0b312c76ca0e16d66b8
Marc <147423007+marcaixs@users.noreply.github.com>
2025-09-03 13:02:32 +0200

    refractor using custom hook useProducts

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,29 +1,19 @@
 import { useState, useEffect } from "react"
+import ProductCard from "../components/ProductCard";
 
 function ShopPage() {
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const {products, error, loading} = useProducts;
 
-   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-      .then((response) => {
-        if (response.status >= 400) {
-            throw new Error("server error");
-        }  
-        return response.json();
-      })
-      .then(data => setProducts(data))
-      .catch(error =>setError(error))
-      .finally(() => setLoading(false));
-  }, []);
-
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>A network error was encountered</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>A network error was encountered</p>;
 
   return (
     <>
-    
+        <div className="products">
+            {products.map((product)=>{
+                return <ProductCard product={product}/>
+            })}
+        </div> 
     </>
   )
 }
