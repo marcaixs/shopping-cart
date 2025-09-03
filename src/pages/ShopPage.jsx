4ea2e896bf2e8094d1dd2085c71721fa9f587112
Marc <147423007+marcaixs@users.noreply.github.com>
2025-09-03 12:35:57 +0200

    add useEffect fetch and update states

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react"
+
 function ShopPage() {
-  
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+   useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => {
+        if (response.status >= 400) {
+            throw new Error("server error");
+        }  
+        return response.json();
+      })
+      .then(data => setProducts(data))
+      .catch(error =>setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>A network error was encountered</p>;
+
   return (
     <>
+    
     </>
   )
 }
