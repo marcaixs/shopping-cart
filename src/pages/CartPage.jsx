fb84c8d263523d541d28515c6ee8aba591106160
Marc <147423007+marcaixs@users.noreply.github.com>
2025-09-04 15:17:30 +0200

    render a map of cart state

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,8 +1,14 @@
-function CartPage() {
+import ProductCard from "../components/ProductCard"
+import '../styles/CartPage.css'
+
+function CartPage({cart, setCart}) {
   
   return (
-    <>
-    </>
+    <div className="cart">
+            {cart.map((product)=>{
+                return <ProductCard product={product} cart={cart} setCart={setCart}/>
+            })}
+    </div> 
   )
 }
 
