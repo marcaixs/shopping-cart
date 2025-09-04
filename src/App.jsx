b78c9e9344f5964c7dd746008c16b25f4b4f17e0
Marc <147423007+marcaixs@users.noreply.github.com>
2025-09-04 14:48:57 +0200

    lift cart state to app.jsx and add the routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ShopPage from "./pages/ShopPage";
+import CartPage from "./pages/CartPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  
+  const [cart, setCart] = useState([]);
+
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage cart={cart} setCart={setCart} />} />
+        <Route path="/cart" element={<CartPage cart={cart} setCart={setCart} />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
