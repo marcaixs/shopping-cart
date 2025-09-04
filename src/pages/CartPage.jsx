0ed33039f1723b0e692215adae0bd5fbcf268362
Marc <147423007+marcaixs@users.noreply.github.com>
2025-09-04 16:20:29 +0200

    add sum of price and message in case cart is empty

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,13 +2,30 @@ import ProductCard from "../components/ProductCard"
 import '../styles/CartPage.css'
 
 function CartPage({cart, setCart}) {
+  const cartItems = cart.reduce((sum, product) => sum + product.price, 0)
   
   return (
     <div className="cart">
-            {cart.map((product)=>{
-                return <ProductCard product={product} cart={cart} setCart={setCart}/>
-            })}
-    </div> 
+      <div className="cart-products">
+       {cart.length > 0 ? (
+        cart.map((product) => (
+          <ProductCard
+          key={product.id}
+          product={product}
+          cart={cart}
+          setCart={setCart}
+       />
+      ))
+    ) : (
+    <p>The kart is empty</p>
+  )}
+        
+  </div>
+    <div className="checkout">
+      <p>Total: {cartItems}</p>
+      <button>Checkout</button>
+    </div>      
+  </div> 
   )
 }
 
