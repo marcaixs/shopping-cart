bdfed9e32f069a61f5c541a2a780582d08b91ba2
Marc <147423007+marcaixs@users.noreply.github.com>
2025-09-04 14:49:51 +0200

    lift cart state to app and call it from props

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,16 +1,19 @@
 import { useState, useEffect } from "react"
+import { Link } from "react-router";
 import ProductCard from "../components/ProductCard";
 import useProducts from "../components/useProducts";
+import '../styles/ShopPage.css'
 
-function ShopPage() {
-  const {products, error, loading} = useProducts();
-  const [cart, setCart] = useState([])
-
+function ShopPage({cart, setCart}) {
+  const { products, error, loading } = useProducts();
+  
   if (loading) return <p>Loading...</p>;
   if (error) return <p>A network error was encountered</p>;
 
   return (
     <>
+        <Link to='/cart'><button>Checkout</button></Link>
+        <p>Cart: {cart.length}</p>
         <div className="products">
             {products.map((product)=>{
                 return <ProductCard product={product} cart={cart} setCart={setCart}/>
