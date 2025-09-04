da54641250bdbc99e376886c5929aa40e2850128
Marc <147423007+marcaixs@users.noreply.github.com>
2025-09-04 14:49:19 +0200

    delete routes and render App

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from "react-router";
-import HomePage from './pages/HomePage.jsx';
-import ShopPage from './pages/ShopPage.jsx';
-import CartPage from './pages/CartPage.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "shop",
-    element: <ShopPage />,
-  },
-  {
-    path: "cart",
-    element: <CartPage />,
-  },
-]);
+import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
